da3f2f7b196a9be00c4855499ff63cbf3533b8f2
정희찬 <anselmo228@naver.com>
2024-04-07 10:50:18 +0900

    PLUG-66 : style - createPostResponseDto 함수 추가로 코드 가독성 향상

--- src/main/java/com/justdo/plug/post/domain/post/service/PostService.java
+++ src/main/java/com/justdo/plug/post/domain/post/service/PostService.java
@@ -23,14 +23,24 @@ public class PostService {
         Post post = postRepository.findById(post_id)
                 .orElseThrow(() -> new RuntimeException("해당 id의 게시글을 찾을 수 없습니다: " + post_id));
 
-        String JsonContent = post.getContent();
+        return createPostResponseDto(post);
+    }
 
-        // JSON 파싱
+    // BLOG003: 블로그 작성
+    public Post save(PostRequestDto requestDto) {
+       
+            Post post = requestDto.toEntity();
+            return postRepository.save(post);
+    }
+
+    // SUB: 게시글 반환 함수
+    private PostResponseDto createPostResponseDto(Post post) throws JSONException {
+        String JsonContent = post.getContent();
         JSONArray jsonArray = new JSONArray(JsonContent);
         List<Object> list = jsonArray.toList();
         Object[] array = list.toArray();
 
-        PostResponseDto responseDto =  PostResponseDto.builder()
+        return PostResponseDto.builder()
                 .post_id(post.getId())
                 .title(post.getTitle())
                 .content(array)
@@ -41,15 +51,6 @@ public class PostService {
                 .member_id(post.getMember_id())
                 .blog_id(post.getBlog_id())
                 .build();
-
-        return responseDto;
-    }
-
-    // BLOG003: 블로그 작성
-    public Post save(PostRequestDto requestDto) {
-       
-            Post post = requestDto.toEntity();
-            return postRepository.save(post);
     }
 
 
