b18832df9d9a92ccc1104c6ec9dbf761fb689e5d
gyeom <rlagusrua3687@gmail.com>
2024-05-10 23:49:29 +0900

    GLUE-247 Feat: 최신 Post, Hashtag 찾는 query method 구현

--- src/main/java/com/justdo/plug/post/domain/posthashtag/repository/PostHashtagRepository.java
+++ src/main/java/com/justdo/plug/post/domain/posthashtag/repository/PostHashtagRepository.java
@@ -1,7 +1,9 @@
 package com.justdo.plug.post.domain.posthashtag.repository;
 
+import com.justdo.plug.post.domain.post.Post;
 import com.justdo.plug.post.domain.posthashtag.PostHashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface PostHashtagRepository extends JpaRepository<PostHashtag, Long>{
     List<PostHashtag> findByPostId(Long postId);
+
+    @Query("SELECT ph FROM PostHashtag ph WHERE ph.post IN :postList")
+    List<PostHashtag> findByPostList(List<Post> postList);
 }
