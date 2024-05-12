89b65f6613550f34b6f16788787d5611c5f78583
gyeom <rlagusrua3687@gmail.com>
2024-05-12 18:36:47 +0900

    GLUE-294 feat: Post의 title, preview, hashtags 기반 검색 API 구현

--- src/main/java/com/justdo/plug/post/domain/post/controller/PostController.java
+++ src/main/java/com/justdo/plug/post/domain/post/controller/PostController.java
@@ -6,11 +6,11 @@ import com.justdo.plug.post.domain.photo.service.PhotoService;
 import com.justdo.plug.post.domain.post.Post;
 import com.justdo.plug.post.domain.post.dto.PostRequestDto;
 import com.justdo.plug.post.domain.post.dto.PostResponseDto;
-import com.justdo.plug.post.domain.post.dto.PostSearchDTO;
 import com.justdo.plug.post.domain.post.dto.PreviewResponse;
 import com.justdo.plug.post.domain.post.dto.PreviewResponse.BlogPostItem;
 import com.justdo.plug.post.domain.post.dto.PreviewResponse.PostItem;
 import com.justdo.plug.post.domain.post.dto.PreviewResponse.PostItemSlice;
+import com.justdo.plug.post.domain.post.dto.SearchResponse;
 import com.justdo.plug.post.domain.post.service.PostService;
 import com.justdo.plug.post.domain.posthashtag.service.PostHashtagService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,7 +21,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONException;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -65,28 +66,16 @@ public class PostController {
         throws JsonProcessingException {
 
         // 1. Post 저장
-        requestDto.setBlogId(blogId);
-
-        // 1-2. Preview 저장
-        String content = requestDto.getContent();
-        String preview = postService.parseContent(content);
-        requestDto.setPreview(preview);
-        Post post = postService.save(requestDto);
-
-        // Post 에서 post_id 받아오기
-        Long postId = post.getId();
+        Post post = postService.save(requestDto, blogId);
 
         // 2. Post_Hashtag 저장
-        List<String> hashtags = requestDto.getHashtags();
-        postHashtagService.createHashtag(hashtags, post);
+        postHashtagService.createHashtag(requestDto.getHashtags(), post);
 
         // 3. Category 저장
-        String name = requestDto.getName(); // '카테고리 명'저장
-        categoryService.createCategory(name, postId);
+        categoryService.createCategory(requestDto.getCategoryName(), post);
 
         // 4. Photo 저장
-        String photoUrl = requestDto.getPhotoUrl();
-        photoService.createPhoto(photoUrl, post);
+        photoService.createPhoto(requestDto.getPhotoUrls(), post);
 
         return "게시글이 성공적으로 업로드 되었습니다";
     }
@@ -100,7 +89,7 @@ public class PostController {
 
     // BLOG005: 게시글 삭제 요청
     @GetMapping("delete/{id}")
-    public String deletePost(@PathVariable String id){
+    public String deletePost(@PathVariable String id) {
         return postService.deletePost(id);
     }
 
@@ -172,12 +161,19 @@ public class PostController {
     }
 
     @Operation(summary = "검색 페이지 - Post 검색을 요청합니다.", description = "Post의 title, content, hashtagName을 기반으로 검색을 진행합니다.")
-    @Parameter(name = "keyword", description = "keyword는 검색어이며, QueryString 입니다.", required = true, example = "종강", in = ParameterIn.QUERY)
+    @Parameters({
+        @Parameter(name = "keyword", description = "keyword는 검색어이며, QueryString 입니다.", required = true, example = "예시", in = ParameterIn.QUERY),
+        @Parameter(name = "page", description = "page : 페이지 번호, Query String입니다.", in = ParameterIn.QUERY),
+        @Parameter(name = "size", description = "size : 페이지 크기, Query String입니다.", in = ParameterIn.QUERY)
+    })
     @GetMapping("search")
-    public List<PostSearchDTO> searchElastic(@RequestParam String keyword) {
-        return postService.searchPost(keyword);
-    }
+    public SearchResponse.SearchInfo searchElastic(@RequestParam String keyword,
+        @RequestParam(value = "page", defaultValue = "0") int page,
+        @RequestParam(value = "size", defaultValue = "10") int size) {
 
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return postService.searchPost(keyword, pageRequest);
+    }
 
 
 }
