c4acf6463aa48bd6c2290860e7a30e502ca2482b
gyeom <rlagusrua3687@gmail.com>
2024-05-16 11:05:15 +0900

    GLUE-307 feat: Comment Entity 작성

--- src/main/java/com/justdo/plug/post/domain/comment/Comment.java
+++ src/main/java/com/justdo/plug/post/domain/comment/Comment.java
@@ -1,5 +1,59 @@
 package com.justdo.plug.post.domain.comment;
 
-public class Comment {
+import com.justdo.plug.post.domain.common.BaseTimeEntity;
+import com.justdo.plug.post.domain.post.Post;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id", nullable = false)
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Comment> children = new ArrayList<>();
+
+    /**
+     * update
+     */
+    public void update(String content) {
+        this.content = content;
+    }
 
 }
