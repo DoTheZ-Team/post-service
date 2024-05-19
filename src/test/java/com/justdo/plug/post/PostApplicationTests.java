a6c2f5451874555b0abeba27b998ebe7194bdf9a
Chung Hee chan <anselmo228@naver.com>
2024-05-19 20:44:11 +0900

    Update PostApplicationTests.java

--- src/test/java/com/justdo/plug/post/PostApplicationTests.java
+++ src/test/java/com/justdo/plug/post/PostApplicationTests.java
@@ -3,7 +3,7 @@ package com.justdo.plug.post;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class PostApplicationTests {
 
 	@Test
