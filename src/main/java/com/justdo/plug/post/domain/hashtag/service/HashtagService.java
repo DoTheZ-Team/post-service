b618b80d3992799ce35a7e4cdd5444005b643d18
gyeom <rlagusrua3687@gmail.com>
2024-06-06 00:11:43 +0900

    :recycle: refactor: 주석 및 공백 제거

--- src/main/java/com/justdo/plug/post/domain/hashtag/service/HashtagService.java
+++ src/main/java/com/justdo/plug/post/domain/hashtag/service/HashtagService.java
@@ -17,22 +17,22 @@ public class HashtagService {
 
     public Hashtag getHashtagIdByName(String hashtagName) {
         Hashtag hashtag = hashtagRepository.findByName(hashtagName);
-        // 만약 사용하려는 해시태그가 없다면 새로운 해시태그 생성 후 사용
+
         return (hashtag == null) ? createNewHashtag(hashtagName) : hashtag;
     }
 
     @Transactional
     public Hashtag createNewHashtag(String hashtagName) {
         Hashtag newHashtag = Hashtag.builder()
-            .name(hashtagName)
-            .build();
+                .name(hashtagName)
+                .build();
 
         return hashtagRepository.save(newHashtag);
     }
 
     public String getHashtagNameById(Long hashtagId) {
         Hashtag hashtag = hashtagRepository.findById(hashtagId)
-            .orElseThrow(() -> new ApiException(ErrorStatus._HASHTAG_NOT_FOUND));
+                .orElseThrow(() -> new ApiException(ErrorStatus._HASHTAG_NOT_FOUND));
         return hashtag.getName();
     }
 
