5e97776c40004c273579cc5bf8286f416f4df792
정희찬 <anselmo228@naver.com>
2024-06-12 11:44:09 +0900

    GLUE-382/불필요한 어노테이션 삭제

--- src/main/java/com/justdo/plug/post/domain/blog/SubscriptionResponse.java
+++ src/main/java/com/justdo/plug/post/domain/blog/SubscriptionResponse.java
@@ -1,15 +1,11 @@
 package com.justdo.plug.post.domain.blog;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 public class SubscriptionResponse {
 
     @Schema(description = "구독 확인 여부 및 프로필 DTO")
-    @AllArgsConstructor
-    @NoArgsConstructor
     @Getter
     public static class SubscribedProfile {
 
